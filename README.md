339a63fac3d65e0bc0bee8800b217751f9a74f76
huanghu <huanghu@xingyun.net>
2023-12-12 14:54:59 +0800

    自动化下载ipinfo.io的免费版数据文件

---
+++ README.md
@@ -0,0 +1,9 @@
+# geoip2
+每日定时发布免费的geoip数据库
+
+ipinfo free database license
+You are required to attribute IPinfo to use our free datasets
+Released under Creative Commons Attribution-ShareAlike 4.0 International License. The attribution requirements can be met by giving our service credit as your data source. Simply place a link to IPinfo on the website, application, or social media account that uses our data.
+
+Provide attributionView license
+<p>IP address data powered by <a href="https://ipinfo.io">IPinfo</a></p>
---
+++ src/ipinfo.io.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+IPINFP_TOKEN=$1
+
+names=(asn.mmdb asn.json.gz asn.csv.gz country.mmdb country.json.gz country.csv.gz country_asn.mmdb country_asn.json.gz country_asn.csv.gz)
+
+body=$(curl "https://api.github.com/repos/cnartlu/geoip2/releases/latest" | grep '"body"' | cut -d '"' -f 4)
+
+newbody="ipinfo文件对应的hash"
+
+dlnames=()
+dlidx=0
+for name in ${names[*]}; do
+    checksums=$(curl "https://ipinfo.io/data/free/${name}/checksums?token=${IPINFP_TOKEN}")
+    md5=$(echo "$checksums" | grep "md5" | cut -d '"' -f 4)
+    sha1=$(echo "$checksums" | grep "sha1" | cut -d '"' -f 4)
+    sha256=$(echo "$checksums" | grep "sha256" | cut -d '"' -f 4)
+    newbody="${newbody}
+name:${name} md5:${md5} sha1:${sha1} sha256:${sha256}"
+    if [ -z "$md5" ]; then
+        continue
+    fi
+    checkmd5=$(echo "$body" | grep "name:$name" | cut -d " " -f 2 | cut -d ":" -f 2)
+    if [ "$checkmd5" == "$md5" ]; then
+        echo "${name} hash equal ${md5}" &>2
+        continue
+    fi
+    dlnames[$dlidx]="$name"
+done
+
+if [ ${#dlnames[*]} -lt 1 ]; then
+    return 0
+fi
+echo "$newbody"
+mkdir -p ./ipinfo
+for name in ${names[*]}; do
+    # wget https://ipinfo.io/data/free/${name}?token=${IPINFP_TOKEN} -O ./ipinfo/${name}
+    touch ./ipinfo/${name}
+    echo "1" >./ipinfo/${name}
+done
+zip -r ipinfo.io.zip ./ipinfo
